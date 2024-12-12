18578a0596532d992dd2d358c60ba6809ae5b75f
V-Fries <vincentfries@proton.me>
2024-12-12 01:54:45 +0100

    revert: Removed take_vec as std::mem::take() serves the same purpose

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,9 @@ mod get_all_uniques;
 mod pipe_line;
 mod result;
 mod scope_guard;
-mod take_vec;
 
 pub use get_all_uniques::GetAllUniques;
 pub use pipe_line::PipeLine;
 pub use result::Result;
 pub use scope_guard::Defer;
 pub use scope_guard::ScopeGuard;
-pub use take_vec::TakeVec;
